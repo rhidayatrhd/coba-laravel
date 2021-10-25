d2e606dec161dec20ded1c23069bfa631bf456bc
rhidayatrhd <rhidayatrhd@gmail.com>
2021-10-25 11:34:48 +0700

    Modify web.php with add link to postcontroller.php

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\PostController;
+use App\Models\Post;
 use Illuminate\Support\Facades\Route;
 use PhpParser\Node\Stmt\If_;
 
@@ -35,64 +37,6 @@ Route::get('/about', function () {
 
 
 
-Route::get('/blog', function () {
-
-    $blog_posts = [
-        [
-            "title"     => "Judul Post Pertama",
-            "slug"      => "judul-post-pertama",
-            "author"    => "Rahmad H",
-            "body"      => "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Omnis delectus ipsa sapiente ipsam accusamus quibusdam, nisi soluta incidunt error id architecto dolores libero eos nulla iure minima illo officiis cum, enim tempora laudantium. Illum repudiandae ullam totam recusandae, facere sit. Quis eveniet fuga odio natus cumque eum ex enim, perferendis culpa quas aut hic vero soluta voluptates doloremque a distinctio voluptatibus adipisci, aperiam nihil. Molestias deserunt repudiandae enim, nisi labore fugiat ad itaque mollitia eveniet placeat ratione sequi numquam libero alias qui eaque, doloribus amet perspiciatis inventore perferendis tempore. Omnis maxime, illum rerum similique labore exercitationem veniam illo enim temporibus."
-        ],
-
-        [
-            "title"     => "Judul Post Kedua",
-            "slug"      => "judul-post-kedua",
-            "author"    => "Franky S",
-            "body"      => "Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit magni consequuntur quisquam eos accusantium, obcaecati unde, itaque pariatur voluptatibus odio necessitatibus magnam ex rerum excepturi commodi maxime eaque eveniet enim? Pariatur doloremque quam iste illo molestias eveniet reiciendis, accusantium amet deleniti autem excepturi assumenda voluptas quidem adipisci, optio aut, atque quos. Eos temporibus at placeat maxime ex, adipisci totam dolorum recusandae sed alias delectus voluptatum odio porro nihil assumenda sit veritatis error blanditiis ad. Rem fuga accusamus rerum quod harum!"
-        ],
-    ];
-
-
-    return view('posts', [
-
-        "title" => "Posts",
-        "posts" => $blog_posts
-
-    ]);
-});
-
-
+Route::get('/blog', [PostController::class, 'index']);
 // Halaman single post
-
-Route::get('posts/{slug}', function ($slug) {
-
-    $blog_posts = [
-        [
-            "title"     => "Judul Post Pertama",
-            "slug"      => "judul-post-pertama",
-            "author"    => "Rahmad H",
-            "body"      => "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Omnis delectus ipsa sapiente ipsam accusamus quibusdam, nisi soluta incidunt error id architecto dolores libero eos nulla iure minima illo officiis cum, enim tempora laudantium. Illum repudiandae ullam totam recusandae, facere sit. Quis eveniet fuga odio natus cumque eum ex enim, perferendis culpa quas aut hic vero soluta voluptates doloremque a distinctio voluptatibus adipisci, aperiam nihil. Molestias deserunt repudiandae enim, nisi labore fugiat ad itaque mollitia eveniet placeat ratione sequi numquam libero alias qui eaque, doloribus amet perspiciatis inventore perferendis tempore. Omnis maxime, illum rerum similique labore exercitationem veniam illo enim temporibus."
-        ],
-
-        [
-            "title"     => "Judul Post Kedua",
-            "slug"      => "judul-post-kedua",
-            "author"    => "Franky S",
-            "body"      => "Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit magni consequuntur quisquam eos accusantium, obcaecati unde, itaque pariatur voluptatibus odio necessitatibus magnam ex rerum excepturi commodi maxime eaque eveniet enim? Pariatur doloremque quam iste illo molestias eveniet reiciendis, accusantium amet deleniti autem excepturi assumenda voluptas quidem adipisci, optio aut, atque quos. Eos temporibus at placeat maxime ex, adipisci totam dolorum recusandae sed alias delectus voluptatum odio porro nihil assumenda sit veritatis error blanditiis ad. Rem fuga accusamus rerum quod harum!"
-        ],
-    ];
-
-    $new_post = [];
-    foreach ($blog_posts as $post) {
-
-        if ($post["slug"] === $slug) {
-            $new_post = $post;
-        }
-    }
-
-    return view('post', [
-        "title" => "Single Post",
-        "post"  => $new_post
-    ]);
-});
+Route::get('/posts/{slug}', [PostController::class, 'show']);
