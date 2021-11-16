261b2ac1429a7330adf6f3617c986ec990e9c6da
rhidayatrhd <rhidayatrhd@gmail.com>
2021-11-16 08:17:27 +0700

    create model file Post.php

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -2,28 +2,26 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
 
-class Post
+class Post extends Model
 {
+    use HasFactory;
 
-    private static $blog_posts = [
-        [
-            "title"     => "Judul Post Pertama",
-            "slug"      => "judul-post-pertama",
-            "author"    => "Rahmad H",
-            "body"      => "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Omnis delectus ipsa sapiente ipsam accusamus quibusdam, nisi soluta incidunt error id architecto dolores libero eos nulla iure minima illo officiis cum, enim tempora laudantium. Illum repudiandae ullam totam recusandae, facere sit. Quis eveniet fuga odio natus cumque eum ex enim, perferendis culpa quas aut hic vero soluta voluptates doloremque a distinctio voluptatibus adipisci, aperiam nihil. Molestias deserunt repudiandae enim, nisi labore fugiat ad itaque mollitia eveniet placeat ratione sequi numquam libero alias qui eaque, doloribus amet perspiciatis inventore perferendis tempore. Omnis maxime, illum rerum similique labore exercitationem veniam illo enim temporibus."
-        ],
+    // protected $fillable = ['title', 'excerpt', 'body'];
+    protected $guarded = ['id'];
 
-        [
-            "title"     => "Judul Post Kedua",
-            "slug"      => "judul-post-kedua",
-            "author"    => "Franky S",
-            "body"      => "Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit magni consequuntur quisquam eos accusantium, obcaecati unde, itaque pariatur voluptatibus odio necessitatibus magnam ex rerum excepturi commodi maxime eaque eveniet enim? Pariatur doloremque quam iste illo molestias eveniet reiciendis, accusantium amet deleniti autem excepturi assumenda voluptas quidem adipisci, optio aut, atque quos. Eos temporibus at placeat maxime ex, adipisci totam dolorum recusandae sed alias delectus voluptatum odio porro nihil assumenda sit veritatis error blanditiis ad. Rem fuga accusamus rerum quod harum!"
-        ],
-    ];
+    protected $with = ['category', 'author'];
 
-    public static function all()
+
+    public function category()
     {
-        return self::$blog_posts;
+        return $this->belongsTo(Category::class);
+    }
+
+    public function author()
+    {
+        return $this->belongsTo(User::class, 'user_id');
     }
 }
