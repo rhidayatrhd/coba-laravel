38a637bbc0322bfceb064db12b2cdbfec13474b5
rhidayatrhd <rhidayatrhd@gmail.com>
2021-11-16 08:18:31 +0700

    Modify app models User.php

--- app/Models/User.php
+++ app/Models/User.php
@@ -15,7 +15,8 @@ class User extends Authenticatable
     /**
      * The attributes that are mass assignable.
      *
-     * @var string[]
+    //  * @var string[]
+     * @var array
      */
     protected $fillable = [
         'name',
@@ -41,4 +42,10 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+
+    public function posts()
+    {
+        return $this->hasMany(Post::class);
+    }
 }
